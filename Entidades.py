136c8a8e42c20803d3c44d2fbf656afb8f629bb3
AlexaRomeroJuPA <alexaromero.hnd@gmail.com>
2026-05-10 12:44:26 -0500

    Create Entidades.py
    Added an abstract base class for system entities.

---
+++ Entidades.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class EntidadBase(ABC):
+    """
+    Clase abstracta base para todas las entidades del sistema.
+    """
+
+    @abstractmethod
+    def mostrar_informacion(self):
+        pass
